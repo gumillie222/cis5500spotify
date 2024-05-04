0561e13324cfdb5bc8796e6343c3a33e7d0187a3
tunamoon <tunamoon@sas.upenn.edu>
2024-05-04 04:41:17 -0400

    commit front for aiden

--- client/src/SearchBar.js
+++ client/src/SearchBar.js
@@ -1,17 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from "axios"
+import { TextField} from '@mui/material';
 
-export default class SearchBar extends React.Component {
-    
+const SearchBar = () => {
+    const [results, setResults] = useState([]);
+    const [text, setText] = useState('');
+    const [one, setOne] = useState('');
+    const [two, setTwo] = useState('');
+    const [three, setThree] = useState('');
 
-    
-
-    render() {
-        return (
+    return (
+        <div>
+            <h1>
+                <input
+                    value={text}
+                    onChange={(e) => {
+                        setText(e.currentTarget.value)
+                    }} />
+                <button onClick={async () => {
+                   const res = await axios.get("(backend stuff)")
+                   console.log(res.data)
+                   setResults(res.data)
+                }}>
+                    Search
+                </button>
+            </h1>
             <div>
-                <h1>
-                    Airbnb: {this.state.concertName}
-                </h1>
+            <TextField
+                placeholder="One"
+                onChange={(event) => {setOne(event.target.value)}}
+            />
+
+            <TextField
+                placeholder="Two"
+                onChange={(event) => {setTwo(event.target.value)}}
+            />
+
+            <TextField
+                placeholder="Three"
+                onChange={(event) => {setThree(event.target.value)}}
+            />
+
+                {JSON.stringify(results)}
             </div>
-        )
-    }
+        </div>
+    )
 }
+
+export default SearchBar
--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client';
 import Map from './Map';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Concert from './Concert';
+import SearchBar from './SearchBar';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path='/' exact element={<Map />} />
         <Route path='/concert/:concertId' element={<Concert />} />
+        <Route path='/SearchBar' exact element={<SearchBar />} />
         
       </Routes>
       
