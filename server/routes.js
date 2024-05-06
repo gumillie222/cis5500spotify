5a334875d48a0b9487ddd4c5f70b3f6159c8f14f
gumillie222 <121700352+gumillie222@users.noreply.github.com>
2024-05-06 17:58:59 -0400

    query 1 fix

--- server/routes.js
+++ server/routes.js
@@ -81,15 +81,25 @@ const topCities = async function (req, res) {
   if (isNaN(limit) || limit < 1) {
     limit = 10;
   }
+  /* commented out because these only need to be executed once in database
+  CREATE INDEX idx_cm_fa ON ConcertMain(formatted_address);
+  CREATE INDEX idx_ca_fa ON ConcertAddr(formatted_address);
+  CREATE INDEX idx_cm_artist ON ConcertMain(artist);
+  CREATE INDEX idx_chm_artist ON ChartMain(artist);
+  CREATE MATERIALIZED VIEW TopCityByArtist AS (
+      SELECT c.city
+      FROM (SELECT city, artist
+        FROM ConcertMain cm JOIN ConcertAddr ca
+        ON ca.formatted_address = cm.formatted_address) c
+      INNER JOIN ChartMain chm
+      ON c.artist = chm.artist
+      GROUP BY c.city
+      ORDER BY COUNT(DISTINCT chm.artist) DESC
+      );
+  */
   pool.query(`
-    SELECT c.city
-    FROM (SELECT city, artist
-      FROM ConcertMain cm JOIN ConcertAddr ca
-      ON ca.formatted_address = cm.formatted_address) c
-    INNER JOIN ChartMain chm
-    ON c.artist = chm.artist
-    GROUP BY c.city
-    ORDER BY COUNT(DISTINCT c.artist) DESC
+    SELECT *
+    FROM TopCityByArtist
     LIMIT ${limit};
 `, (err, data) => {
     if (err || data.length === 0) {
